bf9fbe52b7332a398244e17de5b7aa720b772c4b
jacktone owidi <kingsowidi@gmail.com>
2023-10-23 20:50:47 +0300

    learning python input, string, functions, capitalization and removing white spaces

---
+++ index.py
@@ -0,0 +1,6 @@
+#asking users whats their name, removing white spaces from a str and capitalizing the str
+name = input('what is your name? \n').strip().title()
+print ('have a nice day, '+name+ ' thanks')
+
+
+#this is a comment
