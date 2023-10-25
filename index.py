2652d22779d5457a21ca03daab555a0d79b906e8
jacktone owidi <kingsowidi@gmail.com>
2023-10-25 16:53:16 +0300

    learning about integer

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 #asking users whats their name, removing white spaces from a str and capitalizing the str
-name = input('what is your name? \n').strip().title()
-print ('have a nice day, '+name+ ' thanks')
+#name = input('what is your name? \n').strip().title()
+#print ('have a nice day, '+name+ ' thanks')
 
 
 #this is a comment
+x = int (input('whats x '))
+y = int (input('whats y '))
+print( x + y )
